da02b49f5e366f7230731d61c47a8eecddfa1062
NicholasRomanov <nicholasromanov51@gmail.com>
2022-04-16 14:38:49 +0700

    rumus segitiga js

--- segitiga.js
+++ segitiga.js
@@ -1,5 +1,11 @@
+/**
+ * PSEUDOCODE
+ * STORE alas WITH ANY NUMBER
+ * STORE tinggi WITH ANY NUMBER
+ * SET luas WITH alas MULTIPLY tinggi
+ * DISPLAY luas
+ */
 var alas = 5;
 var tinggi = 2;
-
 var luas = 1/2*alas*tinggi;
 console.log(luas);
